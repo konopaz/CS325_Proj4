bde2fc25695573010315bcb205666725b53af6cc
Zac Konopa <konopaz@onid.oregonstate.edu>
2015-03-09 21:53:33 -0700

    input file parsing done
    Parse the input file into a matrix and populate the matrix with the
    distances between each city.

--- tsp.py
+++ tsp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, os, getopt
+import sys, os, getopt, math
 
 def printHelp():
   print
@@ -16,6 +16,13 @@ def printHelp():
   print "third integers should be the coordinates of the city."
   print
 
+def dist(a,b):
+    # a and b are integer pairs (each representing a point in a 2D, integer grid)
+    # Euclidean distance rounded to the nearest integer:
+    dx = a[0]-b[0]
+    dy = a[1]-b[1]
+    #return int(math.sqrt(dx*dx + dy*dy)+0.5) # equivalent to the next line
+    return int(round(math.sqrt(dx*dx + dy*dy)))
 def main(argv):
 
   try:
@@ -38,13 +45,31 @@ def main(argv):
     exit(1)
 
  
+  cities = []
+  citiesMatrix = {}
   inputFile = open(args[0])
 
-  #while 1:
+  while 1:
 
-    #coinline = inputFile.readline()
-    if not coinline:
+    line = inputFile.readline()
+    if not line:
       break
+
+    (cityid, xcoord, ycoord) = line.rsplit()
+    cities.append((cityid, int(xcoord), int(ycoord)))
+
+  for city1 in cities:
+    citiesMatrix[city1[0]] = {}
+
+    for city2 in cities:
+      if city1[0] == city2[0]:
+        d = 0
+      else:
+        d = dist((city1[1], city1[2]), (city2[1], city2[2]))
+
+      citiesMatrix[city1[0]][city2[0]] = d
+
+  print citiesMatrix
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
